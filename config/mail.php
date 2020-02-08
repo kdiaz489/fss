274c8e17dd472e83c3ba6b31336ce50d5bc27186
Karen Diaz <kstefdiaz@gmail.com>
2020-02-07 17:00:57 -0800

    email change

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'mg.fillstorship.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'reply@mg.fillstorship.com'),
         'name' => env('MAIL_FROM_NAME', 'FillStorShip'),
     ],
 
